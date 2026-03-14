473606912eb91fab3492f55ad9f75091370038e1
Ganesh Samant <ganeshsamant@gmail.com>
2026-03-15 01:43:16 +0530

    feat: install Space Grotesk and Inter fonts via @fontsource

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/space-grotesk/400.css';
+import '@fontsource/space-grotesk/700.css';
+
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
